6521539cfa29ea422973e513e7896412d1a433d4
Saverio Casabianca <Saveriocoppola97@virgilio.it>
2026-03-13 16:15:22 +0100

    final exercises

--- js/D5.js
+++ js/D5.js
@@ -6,24 +6,28 @@ REGOLE
 - Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
 - Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
 */
-
 /* ESERCIZIO 1
     Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
 */
 const pets = ["dog", "cat", "hamster", "redfish"];
-
+console.log(pets);
 /* ESERCIZIO 2
     Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
 */
-
+pets.sort();
 /* ESERCIZIO 3
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
-
+const inverted = ["dog", "cat", "hamster", "redfish"];
+inverted.reverse();
+console.log(inverted);
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
 */
-
+const ultimo = ["dog", "cat", "hamster", "redfish"];
+inverted.shift();
+inverted.push(ultimo);
+console.log(ultimo);
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
 */
@@ -47,11 +51,28 @@ const cars = [
     trims: ["life", "style", "r-line"],
   },
 ];
+cars[0].licensePlate = "GF483DA";
+cars[1].licensePlate = "CA948HD";
+cars[2].licensePlate = "FE125KB";
+console.log(cars);
 
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
+cars.push({
+  brand: "Alfa-Romeo",
+  model: "Giuia",
+  color: "Red",
+  trims: ["allure", "Quadrifoglio-verde"],
+  licensePlate: "FE284GH",
+});
+// console.log(cars); inserimento riuscito
+
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop();
+}
+console.log(cars);
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
